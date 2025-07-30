95a81e4ed8827193f6bae3fb0d36984631ef7f32
Monica Rico-Antonio <ricomonica1108@gmail.com>
2025-07-30 15:22:43 -0700

    playGame function created and declares winner after a 5 round match

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 console.log("Hello World");
 console.log("Hello Sunshine, The Earth Says Hello!");
 
+
 //Two Global variables to keep track of scores.
-let computerScore = 0;
-let humanScore = 0;
+var computerScore = 0;
+var humanScore = 0;
 
 //Create new function named: getComputerChoice
 //getComputerChoice will randomly return one of 3 choices: Rock, Paper or Scissors in string form
@@ -39,33 +40,58 @@ function getHumanchoice(){
 //Make function's humanChoice lower case-insensitive
 //Increment the humanScore and computerScore vars based on round winner.
 function playRound(humanChoice,computerChoice){
-	console.log(humanChoice.toLowerCase())
-	console.log(computerChoice.toLowerCase())
+	console.log("User choice: " + humanChoice.toLowerCase())
+	console.log("Computer Choice: " + computerChoice.toLowerCase())
 	//If both players tie, no points
 	if((humanChoice.toLowerCase() === 'rock' && computerChoice.toLowerCase() === 'rock') ||
-	(humanChoice.toLowerCase() === 'paper' && computerChoice.toLowerCase() === 'paper') ||
-	(humanChoice.toLowerCase() === 'scissor' && computerChoice.toLowerCase() === 'scissor')){
+		(humanChoice.toLowerCase() === 'paper' && computerChoice.toLowerCase() === 'paper') ||
+		(humanChoice.toLowerCase() === 'scissor' && computerChoice.toLowerCase() === 'scissor')){
 		console.log("It's a tie, no point!")
 	}
 
 	//if computer wins, computer +1
-	if((humanChoice.toLowerCase() === 'rock' && computerChoice.toLowerCase() === 'paper') ||
-	(humanChoice.toLowerCase() === 'paper' && computerChoice.toLowerCase() === 'scissor') ||
-	(humanChoice.toLowerCase() === 'scissor' && computerChoice.toLowerCase() === 'rock')){
+	else if((humanChoice.toLowerCase() === 'rock' && computerChoice.toLowerCase() === 'paper') ||
+		(humanChoice.toLowerCase() === 'paper' && computerChoice.toLowerCase() === 'scissor') ||
+		(humanChoice.toLowerCase() === 'scissor' && computerChoice.toLowerCase() === 'rock')){
 		computerScore += 1;
 		console.log("You lose, Computer wins!");
 	}
 
 	//if human wins, human +1
-	if((humanChoice.toLowerCase() === 'rock' && computerChoice.toLowerCase() === 'scissor') ||
-	(humanChoice.toLowerCase() === 'paper' && computerChoice.toLowerCase() === 'rock') ||
-	(humanChoice.toLowerCase() === 'scissor' && computerChoice.toLowerCase() === 'paper')){
+	else if((humanChoice.toLowerCase() === 'rock' && computerChoice.toLowerCase() === 'scissor') ||
+		(humanChoice.toLowerCase() === 'paper' && computerChoice.toLowerCase() === 'rock') ||
+		(humanChoice.toLowerCase() === 'scissor' && computerChoice.toLowerCase() === 'paper')){
 		humanScore += 1;
 		console.log("You win, Computer loss!");
 	}
 }
 
-let humanSelection = getHumanchoice();
-let computerSelection = getComputerChoice();
 
-playRound(humanSelection,computerSelection);
+//Create a function that declares the winner of a 5-round match
+function playGame(){
+
+	//While the score variables are less than 5, play a round
+	while(computerScore < 5 && humanScore < 5){
+		//Get both user and computer choices
+		let humanSelection = getHumanchoice();
+		let computerSelection = getComputerChoice();
+
+		//play a Round
+		playRound(humanSelection,computerSelection);
+
+	}
+	
+	//if the user wins
+	if(humanScore === 5){
+		console.log("The winner is the User! Congrats!");
+		console.log("Final Scores: Computer: " + computerScore + " v User: " + humanScore);
+
+	}
+	if(computerScore === 5){
+		console.log("The winner is the Computer! You lose!");
+		console.log("Final Scores: Computer: " + computerScore + " v User: " + humanScore);
+
+	}
+}
+
+playGame();
